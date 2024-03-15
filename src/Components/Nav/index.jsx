e8e14e2a597357d3427b45e48f0d775cb8578eb4
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-03-15 11:47:13 +0100

    Adds nav component

---
+++ src/Components/Nav/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import './index.css';
+
+
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/home" className="nav-link">The ShopNet</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className="nav-link">Contact</NavLink>
+          </li>
+        </ul>
+        <div className='search-container'>
+          <input type='search' placeholder='Search a product' className='search'></input>
+          <button type='submit'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className='cart-container'>
+          <FontAwesomeIcon icon={faCartShopping} />
+        </div>
+      </nav>
+    </div>
+  ) 
+}
+
+export default Nav;
