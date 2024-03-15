c889b08a5fcdd07abc51dbcf5fdcc4282a98a192
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-03-15 11:46:39 +0100

    Adds layout component

---
+++ src/Components/Layout/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer";
+import Header from "../Header";
+import "../Layout/index.css";
+import "./index.css";
+
+  
+function Layout() {
+  return (
+    <React.Fragment>
+      < Header />
+      < Outlet className="page-body"/>
+      < Footer />
+    </React.Fragment>
+  )
+}
+
+export default Layout;
