50f6547d547f4c40440edd1370e8f523446eb9f2
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-03-15 11:48:19 +0100

    Adds useFetch hook

---
+++ src/hooks/useFetch/index.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import { API_PRODUCTS } from '../../shared/apis';
+
+export function useFetch() {
+  const[products, setProducts] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getData(){
+      try{
+        setIsError(false);
+        setIsLoading(true);
+        
+        const response = await fetch(API_PRODUCTS);
+        const json = await response.json();
+        setProducts(json.data);
+
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+
+    getData();
+
+  }, []);
+  return { products, isLoading, isError};
+};
