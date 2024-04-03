50add0b80111bab149ad79dcf62671eb7c3d85ea
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-03 09:39:49 +0200

    Adds props to products component

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,21 +1,14 @@
-import {useFetch} from "../../hooks/useFetch";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 
-export function Products() { 
-  const { products, isLoading, isError} = useFetch();
-
-  if(isLoading){
-    return <div>Loading products...</div>
-  }
-  if(isError){
-    return <div>Error loading products</div>
-  }
+export function Products({ products }) { 
+  
   return (
   <div className="page-body products-container">
     {products.map((product) => (
       <div key={product.id} className="product-card">
-        <h2>{product.title}</h2>
+        <h2 className="product-title">{product.title}</h2>
         <img src={product.image.url} alt={product.title}></img>
         <p>{product.description}</p>
         <p>Price: {product.price}</p>
