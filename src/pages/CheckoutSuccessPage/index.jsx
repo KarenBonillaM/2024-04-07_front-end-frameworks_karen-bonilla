71f8cef9c0b2b24d4350e947fb1c949ba6ba9945
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-03 09:37:00 +0200

    Adds checkout success page

---
+++ src/pages/CheckoutSuccessPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CheckoutSuccessPage() {
+  return (
+    <div>
+      <h1>Thank you for your purchase!</h1>
+    </div>
+  );
+}
+
+export default CheckoutSuccessPage;
