5843fabc3db3edd61750f37424b1ea07c6633181
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-03 09:36:43 +0200

    Adds contact page

---
+++ src/pages/ContactPage/index.jsx
@@ -0,0 +1,13 @@
+import ContactForm from "../../Components/Forms";
+
+function ContactPage() {
+  return (
+    <main className="page-body">
+      <h1>Contact</h1>
+      <p>Get in touch with us</p>
+      <ContactForm />
+    </main>
+  );
+}
+
+export default ContactPage; 
