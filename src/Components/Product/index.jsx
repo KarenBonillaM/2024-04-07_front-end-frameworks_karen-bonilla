b3b0fdfe9e5bb416df5d07d45d04b2aae388e307
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-03 09:56:23 +0200

    Adds product component

--- src/Components/Product/index.jsx
+++ src/Components/Product/index.jsx
@@ -1,14 +1,53 @@
-import { Product } from "../ProductComponent";
+import { useFetchProduct } from "../../hooks/useFetchProduct";
+import "./index.css"
+import useProductStore from "../../store/index";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import { useEffect } from "react";
 
-function ProductPage() {
+export function Product() {
+  const { product, isLoading, isError } = useFetchProduct();
+  const { products } = useFetch();
+  const addToCart = useProductStore(state => state.addToCart);
+  let {id} = useParams();
+
+  useEffect(() => {
+    useProductStore.setState({products});
+  }, [products]);
+
+  function onAddToCartClick() {
+    addToCart(id);
+  }
+
+  if (isLoading || !product) {
+    return <div>Loading...</div>
+  }
+  
+  if (isError) {
+    return <div>Error</div>
+  }
+
   return (
-    <main className='page-body'>
-      <h1>Product Page</h1>
-      <div>
-        <Product />
-      </div>
-    </main>
-  )
+    <div className="page-body product-container">
+        <div key={product.id} className="singleProduct-card">
+          <h2>{product.title}</h2>
+          <img src={product.image.url} alt={product.title}></img>
+          <p>{product.description}</p>
+          <p>Price: {product.price}</p>
+          <button onClick={onAddToCartClick}>Add to the cart</button>
+          <div>
+            {product.reviews.map((review) => (
+              <div key={review.id} className="review-card">
+                <h3>{review.username}</h3>
+                <p>Rating: {review.rating}</p>
+                <p>{review.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+    </div>
+  );
 }
 
-export default ProductPage;
+
+
