e4541067f524f5db38947c06d16af3e061567f0a
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-03 09:37:14 +0200

    Adds checkout page

---
+++ src/pages/CheckoutPage/index.jsx
@@ -0,0 +1,12 @@
+import Cart from "../../Components/Cart";
+
+function CheckoutPage() {
+  return (
+    <main className="page-body"> 
+      <h1>Checkout</h1>
+      <Cart />
+    </main>
+  )
+}
+
+export default CheckoutPage;
