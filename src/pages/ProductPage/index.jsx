23f76e311574fe43db84d5a17ff8ce73e1da9035
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-03 09:55:10 +0200

    Adds product page

---
+++ src/pages/ProductPage/index.jsx
@@ -0,0 +1,11 @@
+import { Product } from "../../Components/Product";
+
+function ProductPage() {
+  return (
+    <main className="page-body">
+      <Product />
+    </main>
+  );
+}
+
+export default ProductPage; 
