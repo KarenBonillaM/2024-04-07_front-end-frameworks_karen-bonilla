0629214c85f21cf7a41d6ed184b112ef3e1e6b64
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-05 23:30:34 +0200

    Adds discounted price to product page

--- src/Components/Product/index.jsx
+++ src/Components/Product/index.jsx
@@ -4,6 +4,7 @@ import useProductStore from "../../store/index";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import { useEffect } from "react";
+import { UseDiscountedPrice } from "../../hooks/DiscountedPrice";
 
 export function Product() {
   const { product, isLoading, isError } = useFetchProduct();
@@ -28,22 +29,32 @@ export function Product() {
   }
 
   return (
-    <div className="page-body product-container">
+    <div className="product-container">
         <div key={product.id} className="singleProduct-card">
-          <h2>{product.title}</h2>
-          <img src={product.image.url} alt={product.title}></img>
-          <p>{product.description}</p>
-          <p>Price: {product.price}</p>
-          <button onClick={onAddToCartClick}>Add to the cart</button>
           <div>
-            {product.reviews.map((review) => (
-              <div key={review.id} className="review-card">
-                <h3>{review.username}</h3>
-                <p>Rating: {review.rating}</p>
-                <p>{review.description}</p>
-              </div>
-            ))}
+            <img src={product.image.url} alt={product.title} className="single-product-image"></img>
           </div>
+          <div className="single-product-details">
+            <div className="product-info">
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+              <UseDiscountedPrice price={product.price} discount={product.discountedPrice}/>
+              <p className="discounted-price">Discounted Price: {product.discountedPrice}</p>
+              <p className="price">Price: {product.price}</p>
+              <button onClick={onAddToCartClick}>Add to the cart</button>
+            </div>
+            <div className="reviews-container">
+              <h3>Reviews</h3>
+              {product.reviews.map((review) => (
+                <div key={review.id} className="review-cart">
+                  <h3>{review.username}</h3>
+                  <p>Rating: {review.rating}</p>
+                  <p>{review.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          
         </div>
     </div>
   );
