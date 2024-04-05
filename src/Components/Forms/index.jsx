f59d3cffdef94d5a425f846a85bd6f057fe8110f
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-04-05 23:33:10 +0200

    Imports index.css to form component

--- src/Components/Forms/index.jsx
+++ src/Components/Forms/index.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import './index.css'
 
 
 const schema = yup.object({
@@ -32,108 +33,49 @@ function ContactForm() {
     console.log(data);
   }
 
-
-
-
-
-  // const [fullName, setFullName] = useState("");
-  // const [subject, setSubject] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [message, setMessage] = useState("");
-
-  // function onFormSubmit(event) {
-  //   event.preventDefault();
-
-  //   const body = {
-  //     fullName,
-  //     subject,
-  //     email,
-  //     message
-  //   };
-
-  //   fetch('https://v2.api.noroff.dev/online-shop', {
-  //     method: 'POST',
-  //     body: JSON.stringify(body),
-  //   });
-  // }
-
-
-  // function onTextInputChange(event) {
-  //   const value = event.target.value;
-  //   if (event.target.name === 'full-name') {
-  //     setFullName(value);
-  //   }
-  //   if (event.target.name === 'subject') {
-  //     setSubject(value);
-  //   }
-  //   if (event.target.name === 'email') {
-  //     setEmail(value);
-  //   }
-  //   if (event.target.name === 'message') {
-  //     setMessage(value);
-  //   }
-  // }
-
   return (
     <div>
-       {/* <div>
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor='full-name'>Full Name</label>
-        <input 
-        name='full-name'
-        value={fullName}
-        onChange={onTextInputChange}
-        type="text" 
-        placeholder="Full Name" />
-        <label htmlFor='subject'>Subject</label>
-        <input 
-        name='subject'
-        value={subject}
-        onChange={onTextInputChange}
-        type="text" 
-        placeholder="Subject" />
-        <label htmlFor='email'>Email</label>
-        <input 
-        name='email'
-        value={email}
-        onChange={onTextInputChange}
-        type="email" 
-        placeholder="Email" />
-        <label htmlFor='body-email'>Message</label>
-        <textarea 
-        name='message'
-        value={message}
-        onChange={onTextInputChange}
-        placeholder="Message" />
-        <button type="submit">Submit</button>
-      </form>
-    </div> */}
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor='full-name'>Name</label>
-        <input name='full-name' 
-        placeholder='Name'{...register("fullName")} />
-        <p>{errors.fullName?.message}</p>
-        <label htmlFor='subject'>Subject</label>
-        <input 
-        name='subject'
-        placeholder='subject'{...register("subject")} />
-        <p>{errors.subject?.message}</p>
-        <label htmlFor='email'>Email</label>
-        <input 
-        name='email'
-        placeholder='Email'{...register("email")} />
-        <p>{errors.email?.message}</p>
-        <label htmlFor='body'>Message</label>
-        <textarea 
-        name='body'
-        placeholder='message'{...register("body")} />
-        <p>{errors.body?.message}</p>
-        <input type="submit" />
-      </form>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)} className='contact-form'>
+          <div>
+            <div className='form-element'>
+              <label htmlFor='full-name'>Name</label>
+              <input name='full-name' 
+              placeholder='Alba Andersson'{...register("fullName")} />
+            </div>
+            <p>{errors.fullName?.message}</p>
+          </div>
+          <div>
+            <div className='form-element'>
+              <label htmlFor='subject'>Subject</label>
+              <input 
+              name='subject'
+              placeholder='Subject'{...register("subject")} />
+            </div>
+            <p>{errors.subject?.message}</p>
+          </div>
+          <div>
+            <div className='form-element'>
+              <label htmlFor='email'>Email</label>
+              <input 
+              name='email'
+              placeholder='Email'{...register("email")} />
+            </div>
+            <p>{errors.email?.message}</p>
+          </div>
+          <div>
+            <div className='form-element'>
+              <label htmlFor='body'>Message</label>
+              <textarea 
+              name='body'
+              placeholder='Message'{...register("body")} />
+            </div>
+            <p>{errors.body?.message}</p>
+          </div>
+          <input type="submit" className='submit'/>
+        </form>
+      </div>
     </div>
-    </div>
-   
   );
 }
 
